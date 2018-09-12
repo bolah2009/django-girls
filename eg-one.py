994223f67a66983aea4c63cd3bf8d6deec6f4d33
bolah2009 <bolah2009@github.com>
2018-09-12 08:46:27 +0100

    First commit

---
+++ eg-one.py
@@ -0,0 +1,78 @@
+# Code from django girls tutorial 
+# https://tutorial.djangogirls.org/en/
+# Make comments with the "#" symbol
+
+
+# 1.0 A simple print statement
+# print('Hello, Django girls!')
+
+# 1.1 using If … elif … else
+
+# 1.1.1 if
+# if 3 > 2:
+#     print("It works")
+
+# 1.1.2 if else
+# if 5 > 2:
+#     print('5 is indeed greater than 2')
+# else:
+#     print('5 is not greater than 2')
+
+# 1.1.2 if elif else
+# name = 'Sonja'
+# if name == 'Ola':
+#     print('Hey Ola!')
+# elif name == 'Sonja':
+#     print('Hey Sonja!')
+# else:
+#     print('Hey anonymous!')
+
+# 1.2.0 Functions
+# def hi():
+#     print('Hi there!')
+#     print('How are you?')
+# hi()
+
+# 1.2.1 Functions with parameters
+# def hi(name):
+#     if name == 'Ola':
+#         print('Hi Ola!')
+#     elif name == 'Sonja':
+#         print('Hi Sonja!')
+#     else:
+#         print('Hi anonymous!')
+# hi('Sonja')
+
+# 1.2.2 Functions with parameters (NEVER REPEAT YOUR CODE!)
+# def hi(name):
+#     print('Hi ' + name + '!')
+
+# hi("Rachel")
+
+# 1.3.1 Loop (for loop) 
+# girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+
+# for name in girls:
+#     #hi(name)
+#     print('name')
+
+# 1.3.1 Loop (for loop range)
+# range is a function that creates a list of numbers following one after the other (these numbers are provided by you as parameters).
+
+for i in range(1, 6):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
